0d61f739fcef89526ac500551262361f3e0cb897
oksana-chornobai <chernobay.oks@icloud.com>
2025-11-12 10:05:06 +0200

    hooks

---
+++ app/not-found.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import css from './page.module.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/');
+    }, 3000);
+
+    return () => clearTimeout(timer);
+    
+  }, [router]);
+
+  return (
+    <div>
+      <h1 className={css.title}>404 - Page not found</h1>
+      <p className={css.description}>
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <p className={css.description}>
+        You will be redirected to the homepage in a few seconds…
+      </p>
+    </div>
+  );
+}
